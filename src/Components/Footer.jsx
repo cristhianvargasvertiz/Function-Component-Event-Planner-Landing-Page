9895d32f1d2c67c9519dc57681593c5cfcd68184
Cristhian Vargas <cristhianvar09@gmail.com>
2024-08-29 07:59:37 -0500

    Setup Lineal Gradient

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const Footer = () => {
+    return (
+        <>
+ <footer>
+ <p>&copy; Cristhian_Vargas_Vertiz. All rights reserved.</p>
+ </footer>
+        </>
+    )}
+
+export default Footer
